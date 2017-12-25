f89230c402a65390dfa4c1a647568cb693797769
dvasmart <dvasmart@gmail.com>
2017-12-25 22:58:58 +0200

    lesson-final 2 save

---
+++ homework/js/common.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+  $('.slider-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true
+  });
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+})
