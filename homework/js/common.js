29c064d5c350dddc589d12f172d8ed11602b239f
dvasmart <dvasmart@gmail.com>
2017-12-27 01:10:10 +0200

    Lesson-final 3 save

--- homework/js/common.js
+++ homework/js/common.js
@@ -16,3 +16,17 @@ $(document).ready(function(){
     asNavFor: '.slider-nav'
   });
 })
+
+$(function(){
+  $('ul.tabs li:first').addClass('active');
+  $('.block article').hide();
+  $('.block article:first').show();
+  $('ul.tabs li').on('click',function(){
+    $('ul.tabs li').removeClass('active');
+    $(this).addClass('active')
+    $('.block article').hide();
+    var activeTab = $(this).find('a').attr('href');
+    $(activeTab).show();
+    return false;
+  });
+})
